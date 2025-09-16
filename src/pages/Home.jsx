b669665062e227dc20e813d8a710a55911674f09
Vishal Singh <126417458+vshl2706@users.noreply.github.com>
2025-09-17 00:59:47 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,193 +11,29 @@ import TimelineSection from "../components/core/HomePage/Timeline";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-// function Home() {
-//   return (
-//     <div>
-//       <div className="relative mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 text-white">
-//         <Link to={"/signup"}>
-//           <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
-//             <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
-//               <p>Become an Instructor</p>
-//               <FaArrowRight />
-//             </div>
-//           </div>
-//         </Link>
-
-//         <div className="text-center text-4xl font-semibold">
-//           Empower Your Future with
-//           <HighlightText text={"Coding Skills"} />
-//         </div>
-
-//         <div className="-mt-3 w-[90%] text-center text-lg font-bold text-richblack-300">
-//           With our online coding courses, you can learn at your own pace, from
-//           anywhere in the world, and get access to a wealth of resources,
-//           including hands-on projects, quizzes, and personalized feedback from
-//           instructors.
-//         </div>
-
-//         <div className="mt-8 flex flex-row gap-7">
-//           <CTAButton active={true} linkto={"/signup"}>
-//             Learn More
-//           </CTAButton>
-//           <CTAButton active={false} linkto={"/login"}>
-//             Book a Demo
-//           </CTAButton>
-//         </div>
-
-//         <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
-//           <video
-//             className="shadow-[20px_20px_rgba(255,255,255)]"
-//             muted
-//             loop
-//             autoPlay
-//           >
-//             <source src={Banner} type="video/mp4" />
-//           </video>
-//         </div>
-
-//         <div>
-//           <CodeBlocks
-//             position={"lg:flex-row"}
-//             heading={
-//               <div className="text-4xl font-semibold">
-//                 Unlock your
-//                 <HighlightText text={"coding potential"} /> with our online
-//                 courses.
-//               </div>
-//             }
-//             subheading={
-//               "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
-//             }
-//             ctabtn1={{
-//               btnText: "Try it Yourself",
-//               link: "/signup",
-//               active: true,
-//             }}
-//             ctabtn2={{
-//               btnText: "Learn More",
-//               link: "/signup",
-//               active: false,
-//             }}
-//             codeColor={"text-yellow-25"}
-//             codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
-//             backgroundGradient={<div className="codeblock1 absolute"></div>}
-//           />
-//         </div>
-
-//         <div>
-//           <CodeBlocks
-//             position={"lg:flex-row-reverse"}
-//             heading={
-//               <div className="w-[100%] text-4xl font-semibold lg:w-[50%]">
-//                 Start
-//                 <HighlightText text={"coding in seconds"} />
-//               </div>
-//             }
-//             subheading={
-//               "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
-//             }
-//             ctabtn1={{
-//               btnText: "Continue Lesson",
-//               link: "/signup",
-//               active: true,
-//             }}
-//             ctabtn2={{
-//               btnText: "Learn More",
-//               link: "/signup",
-//               active: false,
-//             }}
-//             codeColor={"text-white"}
-//             codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
-//             backgroundGradient={<div className="codeblock2 absolute"></div>}
-//           />
-//         </div>
-
-//         <ExploreMore />
-//       </div>
-
-//       <div className="bg-pure-greys-5 text-richblack-700">
-//         <div className="homepage_bg h-[320px]">
-//           <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8">
-//             <div className="lg:h-[150px]"></div>
-//             <div className="flex flex-row gap-7 text-white lg:mt-8">
-//               <CTAButton active={true} linkto={"/signup"}>
-//                 <div className="flex items-center gap-2">
-//                   Explore Full Catalog
-//                   <FaArrowRight />
-//                 </div>
-//               </CTAButton>
-//               <CTAButton active={false} linkto={"/login"}>
-//                 Learn More
-//               </CTAButton>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 ">
-//           <div className="mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row lg:gap-0">
-//             <div className="text-4xl font-semibold lg:w-[45%] ">
-//               Get the skills you need for a{" "}
-//               <HighlightText text={"job that is in demand."} />
-//             </div>
-//             <div className="flex flex-col items-start gap-10 lg:w-[40%]">
-//               <div className="text-[16px]">
-//                 The modern StudyNotion is the dictates its own terms. Today, to
-//                 be a competitive specialist requires more than professional
-//                 skills.
-//               </div>
-//               <CTAButton active={true} linkto={"/signup"}>
-//                 <div className="">Learn More</div>
-//               </CTAButton>
-//             </div>
-//           </div>
-
-//           <TimelineSection />
-
-//           <LearningLanguageSection />
-//         </div>
-//       </div>
-
-//       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
-//         <InstructorSection />
-
-//         <h1 className="text-center text-4xl font-semibold mt-8">
-//           Reviews from other learners
-//         </h1>
-//         <ReviewSlider />
-//       </div>
-
-//       {/* Footer */}
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default Home;
-
 function Home() {
   return (
     <div>
-      {/* Hero Section */}
-      <div className="relative mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 text-gray-100">
+      <div className="relative mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 text-white">
         <Link to={"/signup"}>
-          <div className="group mx-auto mt-16 w-fit rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-[2px] font-bold text-white shadow-md hover:scale-95 transition-all duration-300">
-            <div className="flex flex-row items-center gap-2 rounded-full bg-gray-900 px-10 py-[6px] transition-all duration-200 group-hover:bg-gray-800">
+          <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
+            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
               <p>Become an Instructor</p>
               <FaArrowRight />
             </div>
           </div>
         </Link>
 
-        <div className="text-center text-4xl font-extrabold">
-          Empower Your Future with{" "}
+        <div className="text-center text-4xl font-semibold">
+          Empower Your Future with
           <HighlightText text={"Coding Skills"} />
         </div>
 
-        <div className="-mt-3 w-[90%] text-center text-lg font-medium text-gray-300">
+        <div className="-mt-3 w-[90%] text-center text-lg font-bold text-richblack-300">
           With our online coding courses, you can learn at your own pace, from
-          anywhere in the world, and get access to hands-on projects, quizzes,
-          and personalized feedback from instructors.
+          anywhere in the world, and get access to a wealth of resources,
+          including hands-on projects, quizzes, and personalized feedback from
+          instructors.
         </div>
 
         <div className="mt-8 flex flex-row gap-7">
@@ -209,9 +45,9 @@ function Home() {
           </CTAButton>
         </div>
 
-        <div className="mx-3 my-7 rounded-lg overflow-hidden shadow-2xl shadow-purple-300/30">
+        <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
           <video
-            className="rounded-lg"
+            className="shadow-[20px_20px_rgba(255,255,255)]"
             muted
             loop
             autoPlay
@@ -220,14 +56,14 @@ function Home() {
           </video>
         </div>
 
-        {/* First Code Block */}
         <div>
           <CodeBlocks
             position={"lg:flex-row"}
             heading={
-              <div className="text-4xl font-bold">
-                Unlock your{" "}
-                <HighlightText text={"coding potential"} /> with our courses.
+              <div className="text-4xl font-semibold">
+                Unlock your
+                <HighlightText text={"coding potential"} /> with our online
+                courses.
               </div>
             }
             subheading={
@@ -243,21 +79,19 @@ function Home() {
               link: "/signup",
               active: false,
             }}
-            codeColor={"text-cyan-300"}
-            codeblock={`<!DOCTYPE html>\n<html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
-            backgroundGradient={
-              <div className="absolute inset-0 bg-gradient-to-r from-blue-500 to-purple-600 opacity-20 blur-3xl rounded-full"></div>
-            }
+            codeColor={"text-yellow-25"}
+            codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+            backgroundGradient={<div className="codeblock1 absolute"></div>}
           />
         </div>
 
-        {/* Second Code Block */}
         <div>
           <CodeBlocks
             position={"lg:flex-row-reverse"}
             heading={
-              <div className="w-full text-4xl font-bold lg:w-[50%]">
-                Start <HighlightText text={"coding in seconds"} />
+              <div className="w-[100%] text-4xl font-semibold lg:w-[50%]">
+                Start
+                <HighlightText text={"coding in seconds"} />
               </div>
             }
             subheading={
@@ -273,19 +107,16 @@ function Home() {
               link: "/signup",
               active: false,
             }}
-            codeColor={"text-pink-300"}
-            codeblock={`import React from "react";\nimport CTAButton from "./Button";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
-            backgroundGradient={
-              <div className="absolute inset-0 bg-gradient-to-l from-pink-500 to-indigo-500 opacity-20 blur-3xl rounded-full"></div>
-            }
+            codeColor={"text-white"}
+            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+            backgroundGradient={<div className="codeblock2 absolute"></div>}
           />
         </div>
 
         <ExploreMore />
       </div>
 
-      {/* Mid Section */}
-      <div className="bg-gradient-to-br from-gray-50 to-gray-200 text-gray-800">
+      <div className="bg-pure-greys-5 text-richblack-700">
         <div className="homepage_bg h-[320px]">
           <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8">
             <div className="lg:h-[150px]"></div>
@@ -303,33 +134,34 @@ function Home() {
           </div>
         </div>
 
-        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center gap-8 ">
-          <div className="mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row">
-            <div className="text-4xl font-bold lg:w-[45%] ">
+        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 ">
+          <div className="mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row lg:gap-0">
+            <div className="text-4xl font-semibold lg:w-[45%] ">
               Get the skills you need for a{" "}
               <HighlightText text={"job that is in demand."} />
             </div>
             <div className="flex flex-col items-start gap-10 lg:w-[40%]">
-              <div className="text-[16px] text-gray-600">
-                The modern StudyNotion dictates its own terms. Today, to be a
-                competitive specialist requires more than professional skills.
+              <div className="text-[16px]">
+                The modern StudyNotion is the dictates its own terms. Today, to
+                be a competitive specialist requires more than professional
+                skills.
               </div>
               <CTAButton active={true} linkto={"/signup"}>
-                <div>Learn More</div>
+                <div className="">Learn More</div>
               </CTAButton>
             </div>
           </div>
 
           <TimelineSection />
+
           <LearningLanguageSection />
         </div>
       </div>
 
-      {/* Instructor + Reviews */}
-      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center gap-8 bg-gradient-to-r from-indigo-900 via-purple-900 to-black text-white rounded-2xl p-8 shadow-xl">
+      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
         <InstructorSection />
 
-        <h1 className="text-center text-4xl font-extrabold mt-8">
+        <h1 className="text-center text-4xl font-semibold mt-8">
           Reviews from other learners
         </h1>
         <ReviewSlider />
